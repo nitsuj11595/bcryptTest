cbc6e850bd50c9b2c32fbc78688a81c67f7254af
justintime4email <justintime4email@gmail.com>
2018-09-14 19:39:59 -0400

    added comments

--- src/Generate.java
+++ src/Generate.java
@@ -2,19 +2,23 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 
 public class Generate {
 
-	public static void main(String[] args) {
-		switch(args.length) {
-		case 2:
-			break;
-		case 1:
-			break;
-		default:
-			System.err.println("Usage:");
-			System.err.println("\tGenerate [password] [salt]");
-			return;
-		}
-		String salt = args.length == 2 ? args[1] : BCrypt.gensalt();
-		System.out.println(BCrypt.hashpw(args[0], salt));
+    public static void main(String[] args) {
+	
+	switch(args.length) {
+	case 2:
+	    break;
+	case 1:
+	    break;
+	default:
+	    System.err.println("Usage:");
+	    System.err.println("\tGenerate [password] [salt]");
+	    return;
 	}
+
+	//Generate a salt if none is specified
+	String salt = args.length == 2 ? args[1] : BCrypt.gensalt();
+	//Print the salted hash string
+	System.out.println(BCrypt.hashpw(args[0], salt));
+    }
 
 }
